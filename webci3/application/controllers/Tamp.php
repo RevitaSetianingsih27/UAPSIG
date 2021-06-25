b08dc8ff8cc9a5385993c4dd4176c5f73614a369
RevitaSetianingsih27 <revita.setianingsih105819@students.unila.ac.id>
2021-06-24 20:45:06 -0700

    commit ke 3

--- webci3/application/controllers/Tamp.php
+++ webci3/application/controllers/Tamp.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Tamp extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,14 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('pages/index');
+		$this->load->view('template/head');
+		$this->load->view('index');
+		$this->load->view('template/foot');
+	}
+	public function map2()
+	{
+		$this->load->view('template/head');
+		$this->load->view('index');
+		$this->load->view('template/foot2');
 	}
 }
---
+++ webci3/application/views/index.php
@@ -0,0 +1,14 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+    <div id="mapid" style="width: 600px; height: 400px;"></div>
+      <!-- /.row (main row) -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
